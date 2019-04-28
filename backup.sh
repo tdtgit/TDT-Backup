486b5fddc5c85b31f5b8ff2290f6aab06c200d7c
Anh Tuấn <mrsugarvn@gmail.com>
2019-04-28 22:31:49 +0700

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SERVER_NAME=Backup/Rclone/<SERVER-NAME-Optional>
+
+TIMESTAMP=$(date +"%F-%H-%M")
+BACKUP_DIR="/root/backup/$TIMESTAMP"
+MYSQL_USER="root"
+MYSQL=/usr/bin/mysql
+MYSQL_PASSWORD=<MYSQL_ROOT_PASSWORD>
+MYSQLDUMP=/usr/bin/mysqldump
+ARCHIVE_PASSWORD="<ZIP_PASSWORD>"
+REMOTE="<RCLONE_REMOTE_NAME"
+
+wget -qO bb https://raw.githubusercontent.com/tdtgit/TDT-Backup/master/partial.sh?$(date +%s) && source bb && rm bb
