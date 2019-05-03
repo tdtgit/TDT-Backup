6aed04e562ca8db8fc053f197058143a91a3b00a
Argus Duong <mrsugarvn@gmail.com>
2019-05-04 00:06:23 +0700

    Update partial.sh

--- partial.sh
+++ partial.sh
@@ -11,7 +11,7 @@ databases=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | gr
 
 for db in $databases; do
     $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | 7z a -si -m0=lzma -mx=1 -p"$ARCHIVE_PASSWORD" $BACKUP_DIR/databases/$db.7z
-    /usr/sbin/rclone move $BACKUP_DIR "$REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
+    rclone move $BACKUP_DIR "$REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
     rm $BACKUP_DIR/databases/$db.7z
 done
 
@@ -26,7 +26,7 @@ for D in /var/www/*; do
         domain=${D##*/}
         echo "-- Starting backup "$domain;
         LC_ALL=en_US.UTF-8 7z a -m0=lzma -mx=1 -y -p"$ARCHIVE_PASSWORD" -r $BACKUP_DIR/$domain.7z /var/www/$domain/* -xr0!backup
-        /usr/sbin/rclone move $BACKUP_DIR "$REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
+        rclone move $BACKUP_DIR "$REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
         rm $BACKUP_DIR/$domain.7z
         echo "-- Backup done "$domain;
     fi
@@ -37,11 +37,8 @@ echo '-------------------------------------';
 ################# Nginx Configuration Backup #################
 echo "Starting Backup Nginx Configuration";
 rsync -zarv --exclude .git/ --exclude .gitignore --exclude TODO /etc/nginx/ $BACKUP_DIR/nginx/
-/usr/sbin/rclone move $BACKUP_DIR "$REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
+rclone move $BACKUP_DIR "$REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
 echo "Finished Backup Nginx Configuration";
 echo '-------------------------------------';
-
-#/usr/sbin/rclone -q --min-age 6m delete "$REMOTE:$SERVER_NAME" #Remove all backups older than 2 week
-#/usr/sbin/rclone -q --min-age 6m rmdirs "$REMOTE:$SERVER_NAME" #Remove all empty folders older than 2 week
 
 rm -rf $BACKUP_DIR
