0b25dba76d917bad6a9ea11f62cc839abbe22aec
Anh Tuan <atuan@TDT-Macbook.local>
2019-05-04 00:33:57 +0700

    Change to remaster branch and clean up

--- backup.sh
+++ backup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-SERVER_NAME=Backup/Rclone/<SERVER-NAME-Optional>
+SERVER_NAME=Backup/Rclone/$(hostname)
 
 TIMESTAMP=$(date +"%F-%H-%M")
 BACKUP_DIR="/root/backup/$TIMESTAMP"
 MYSQL_USER="root"
-MYSQL=/usr/bin/mysql
 MYSQL_PASSWORD=""
-MYSQLDUMP=/usr/bin/mysqldump
-ARCHIVE_PASSWORD="<ZIP_PASSWORD>"
-RCLONE_REMOTE="<RCLONE_REMOTE_NAME"
+ARCHIVE_PASSWORD=""
+RCLONE_REMOTE=""
 
-wget -qO bb https://raw.githubusercontent.com/tdtgit/TDT-Backup/master/partial.sh?$(date +%s) && source bb && rm bb
+wget -qO bb https://raw.githubusercontent.com/tdtgit/TDT-Backup/remaster/partial.sh?$(date +%s) && source bb && rm bb
