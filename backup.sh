972cba1532fdd914b9c3a5ff1d1ae6be1be537e1
Anh Tuan <atuan@TDT-Macbook.local>
2019-05-04 00:23:05 +0700

    First commit

--- backup.sh
+++ backup.sh
@@ -6,9 +6,9 @@ TIMESTAMP=$(date +"%F-%H-%M")
 BACKUP_DIR="/root/backup/$TIMESTAMP"
 MYSQL_USER="root"
 MYSQL=/usr/bin/mysql
-MYSQL_PASSWORD=<MYSQL_ROOT_PASSWORD>
+MYSQL_PASSWORD=""
 MYSQLDUMP=/usr/bin/mysqldump
 ARCHIVE_PASSWORD="<ZIP_PASSWORD>"
-REMOTE="<RCLONE_REMOTE_NAME"
+RCLONE_REMOTE="<RCLONE_REMOTE_NAME"
 
 wget -qO bb https://raw.githubusercontent.com/tdtgit/TDT-Backup/master/partial.sh?$(date +%s) && source bb && rm bb
--- partial.sh
+++ partial.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+
 if [ -z "$BACKUP_DIR" ]; then
-    echo "Wrong config! Please check again."
+    echo "Provide your backup directory in Drive"
+    exit 1
+fi
+
+if [ -z "$MYSQL_USER" ]; then
+    echo "Provide your MySQL root username"
+    exit 1
+fi
+
+if [ -z "$MYSQL_PASSWORD" ]; then
+    echo "Provide your MySQL root password"
+    exit 1
+fi
+
+if [ -z "$RCLONE_REMOTE" ]; then
+    echo "Provide your Rclone remote drive name"
     exit 1
 fi
 
@@ -7,11 +24,11 @@ fi
 mkdir -p "$BACKUP_DIR/databases"
 
 echo "Starting Backup Database";
-databases=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql)"`
+databases=`mysql --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql)"`
 
 for db in $databases; do
-    $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | 7z a -si -m0=lzma -mx=1 -p"$ARCHIVE_PASSWORD" $BACKUP_DIR/databases/$db.7z
-    rclone move $BACKUP_DIR "$REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
+    mysqldump --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | 7z a -si -m0=lzma -mx=1 -p"$ARCHIVE_PASSWORD" $BACKUP_DIR/databases/$db.7z
+    rclone move $BACKUP_DIR "$RCLONE_REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
     rm $BACKUP_DIR/databases/$db.7z
 done
 
@@ -26,7 +43,7 @@ for D in /var/www/*; do
         domain=${D##*/}
         echo "-- Starting backup "$domain;
         LC_ALL=en_US.UTF-8 7z a -m0=lzma -mx=1 -y -p"$ARCHIVE_PASSWORD" -r $BACKUP_DIR/$domain.7z /var/www/$domain/* -xr0!backup
-        rclone move $BACKUP_DIR "$REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
+        rclone move $BACKUP_DIR "$RCLONE_REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
         rm $BACKUP_DIR/$domain.7z
         echo "-- Backup done "$domain;
     fi
@@ -37,7 +54,7 @@ echo '-------------------------------------';
 ################# Nginx Configuration Backup #################
 echo "Starting Backup Nginx Configuration";
 rsync -zarv --exclude .git/ --exclude .gitignore --exclude TODO /etc/nginx/ $BACKUP_DIR/nginx/
-rclone move $BACKUP_DIR "$REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
+rclone move $BACKUP_DIR "$RCLONE_REMOTE:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
 echo "Finished Backup Nginx Configuration";
 echo '-------------------------------------';
 
